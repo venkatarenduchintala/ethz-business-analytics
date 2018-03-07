e78cce1675c059eb772cd43d2c15bda9ecb9cd36
venkatarenduchintala <venkata.subramani@gmail.com>
2018-03-07 17:02:08 +0100

    Initial Version

---
+++ lecture 03/IntroductionToR.R
@@ -0,0 +1,52 @@
+# Arithmetic Operations
+1+2*3;
+3/4 + 2
+2*pi-pi
+2*pi
+pi
+0/0
+7/0
+
+
+#Logical Operators
+3 < 4
+3 >= 4
+3 >= 3.1
+3 == 3.0
+
+
+#Brackets, Comments and Decimal Points
+3*(4+2)
+
+
+#Mathematical Functions
+#Square Root
+sqrt(1+1)
+
+#Logarithm 10
+log10(10*10*10)
+log2(2*2*2)
+log(e)
+sin(0)
+sin(90)
+x <- seq(-3, 7, by = 1/8)
+print(x)
+
+print(abs(3-4*5))
+
+#Variables
+#clearing of variables.
+rm(x)
+x <- 2
+print(x)
+
+
+#Strings
+x <- "Venkat"
+print(x)
+
+
+#Vectors
+y <- numeric(4)
+print(x)
+x <- c(4,0,6)
